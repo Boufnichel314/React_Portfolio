fdaa3f0c78cfdc693dd36169a051b6759300f7a0
Boufnichel314 <boufnichel314@gmail.com>
2022-09-14 11:34:57 +0100

    studies file

--- src/Pages/Studies.js
+++ src/Pages/Studies.js
@@ -1,6 +1,6 @@
 import MotionHoc from "./MotionHoc";
 const DocumentsComponent = () => {
-  return <h1></h1>;
+  return <h1>Studied</h1>;
 };
 const Studies = MotionHoc(DocumentsComponent);
 export default Studies;
