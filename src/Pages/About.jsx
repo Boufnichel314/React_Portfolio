49d381be88ae38dc780746e0ad7ea548aef5e584
Boufnichel314 <boufnichel314@gmail.com>
2022-09-15 17:47:06 +0100

    about changes

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,7 +5,16 @@ const CalenderComponent = () => {
   return (
     <div className="about_container">
       <div className="about_image">
+        <div className="about_name">
+        <h2>Boufnichel</h2>
+        {/* <h3>Yassine</h3> */}
+        </div>
+        <div className="about_description">
+          <p> Hello dear, <br /> I'm a software enineer student, a web application developer, with expertise in reactJs, Laravel and responsive design, so i will definitely help you for whatever you want. <br /> Don't hesitate, and contatct me. </p>
+        </div>
+        <div className="about_icons">
 
+        </div>
       </div>
       <div className="image">
       <div className="img">
